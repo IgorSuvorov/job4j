540ad63f6916cc72c98493ddf5ff3c3ae727a0e7
isuvorov1 <isuvorov1@outlook.com>
2022-12-03 00:11:52 -0500

    priority

--- src/test/java/search/PriorityQueueTest.java
+++ src/test/java/search/PriorityQueueTest.java
@@ -1,2 +1,26 @@
-package search;public class PriorityQueueTest {
+package search;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PriorityQueueTest {
+    @Test
+    public void whenHigherPrioritySecond() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        assertThat(result.getDesc()).isEqualTo("urgent");
+    }
+
+    @Test
+    public void whenHigherPriorityEquals() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 5));
+        queue.put(new Task("middle", 5));
+        Task result = queue.take();
+        assertThat(result.getDesc()).isEqualTo("low");
+    }
 }
