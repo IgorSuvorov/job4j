aab7057aa80acb9e300ea26d752b169e2cccb02b
isuvorov1 <isuvorov1@outlook.com>
2022-12-03 13:16:16 -0500

    2. Идентичные тексты [#212655]

---
+++ src/main/java/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package collection;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        Set<String> check = new HashSet<>();
+        check.addAll(List.of(origin));
+        for (String str : text) {
+            if (!check.contains(str)) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
