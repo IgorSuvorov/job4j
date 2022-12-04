159997183805921bf093c0484ea005eb774aa897
isuvorov1 <isuvorov1@outlook.com>
2022-12-04 13:29:20 -0500

    fixed

--- src/main/java/other/StringRunner.java
+++ src/main/java/other/StringRunner.java
@@ -2,7 +2,7 @@ package other;
 
 public class StringRunner {
     public static void main(String[] args) {
-        String idea = "I like Java! ";
+        String idea = "I like Java!";
         System.out.println(idea);
         idea += " But I am a newbie. ";
         int year = 2022;
